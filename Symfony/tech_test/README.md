1462043c42a3882660c228195947a09713f6e9ba
lionel <lionel.capoen@coding-academy.fr>
2017-01-30 16:04:28 +0100

    added souscritoo page

---
+++ Symfony/tech_test/README.md
@@ -0,0 +1,4 @@
+tech_test
+=========
+
+A Symfony project created on January 29, 2017, 2:36 pm.
---
+++ Symfony/tech_test/src/LC/Tech_testBundle/LCTech_testBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LC\Tech_testBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LCTech_testBundle extends Bundle
+{
+}
